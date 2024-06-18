7d6681145eab70a8018f859b31937bf8989edfe7
tommyrharper <thomasrobertharper@gmail.com>
2024-06-18 13:52:27 +0100

    feat: uncomment part of parse_type

--- src/parser.rs
+++ src/parser.rs
@@ -63,68 +63,68 @@ impl Parser {
 		}
 	}
 
-// 	/// the type string is being validated before it's parsed.
-// 	pub fn parse_type(&self, field_type: &str) -> Result<Type> {
-// 		#[derive(PartialEq)]
-// 		enum State { Open, Close }
+	/// the type string is being validated before it's parsed.
+	pub fn parse_type(&self, field_type: &str) -> Result<Type> {
+		#[derive(PartialEq)]
+		enum State { Open, Close }
 
-// 		let mut lexer = Lexer::new(&self.arena, field_type);
-// 		let mut token = None;
-// 		let mut state = State::Close;
-// 		let mut array_depth = 0;
-// 		let mut current_array_length: Option<u64> = None;
+		// let mut lexer = Lexer::new(&self.arena, field_type);
+		let mut token = None;
+		let mut state = State::Close;
+		let mut array_depth = 0;
+		let mut current_array_length: Option<u64> = None;
 
-// 		while lexer.token != Token::EndOfProgram  {
-// 			let type_ = match lexer.token {
-// 				Token::Identifier => Type::Custom(lexer.token_as_str().to_owned()),
-// 				Token::TypeByte => Type::Byte(lexer.type_size.0),
-// 				Token::TypeBytes => Type::Bytes,
-// 				Token::TypeBool => Type::Bool,
-// 				Token::TypeUint => Type::Uint,
-// 				Token::TypeInt => Type::Int,
-// 				Token::TypeString => Type::String,
-// 				Token::TypeAddress => Type::Address,
-// 				Token::LiteralInteger => {
-// 					let length = lexer.token_as_str();
-// 					current_array_length = Some(length
-// 						.parse()
-// 						.map_err(|_|
-// 							ErrorKind::InvalidArraySize(length.into())
-// 						)?
-// 					);
-// 					lexer.consume();
-// 					continue;
-// 				},
-// 				Token::BracketOpen if token.is_some() && state == State::Close => {
-// 					state = State::Open;
-// 					lexer.consume();
-// 					continue
-// 				}
-// 				Token::BracketClose if array_depth < 10 => {
-// 					if state == State::Open && token.is_some() {
-// 						let length = current_array_length.take();
-// 						state = State::Close;
-// 						token = Some(Type::Array {
-// 							inner: Box::new(token.expect("if statement checks for some; qed")),
-// 							length
-// 						});
-// 						lexer.consume();
-// 						array_depth += 1;
-// 						continue
-// 					} else {
-// 						return Err(ErrorKind::UnexpectedToken(lexer.token_as_str().to_owned(), field_type.to_owned()))?
-// 					}
-// 				}
-// 				Token::BracketClose if array_depth == 10 => {
-// 					return Err(ErrorKind::UnsupportedArrayDepth)?
-// 				}
-// 				_  => return Err(ErrorKind::UnexpectedToken(lexer.token_as_str().to_owned(), field_type.to_owned()))?
-// 			};
+		// while lexer.token != Token::EndOfProgram  {
+		// 	let type_ = match lexer.token {
+		// 		Token::Identifier => Type::Custom(lexer.token_as_str().to_owned()),
+		// 		Token::TypeByte => Type::Byte(lexer.type_size.0),
+		// 		Token::TypeBytes => Type::Bytes,
+		// 		Token::TypeBool => Type::Bool,
+		// 		Token::TypeUint => Type::Uint,
+		// 		Token::TypeInt => Type::Int,
+		// 		Token::TypeString => Type::String,
+		// 		Token::TypeAddress => Type::Address,
+		// 		Token::LiteralInteger => {
+		// 			let length = lexer.token_as_str();
+		// 			current_array_length = Some(length
+		// 				.parse()
+		// 				.map_err(|_|
+		// 					ErrorKind::InvalidArraySize(length.into())
+		// 				)?
+		// 			);
+		// 			lexer.consume();
+		// 			continue;
+		// 		},
+		// 		Token::BracketOpen if token.is_some() && state == State::Close => {
+		// 			state = State::Open;
+		// 			lexer.consume();
+		// 			continue
+		// 		}
+		// 		Token::BracketClose if array_depth < 10 => {
+		// 			if state == State::Open && token.is_some() {
+		// 				let length = current_array_length.take();
+		// 				state = State::Close;
+		// 				token = Some(Type::Array {
+		// 					inner: Box::new(token.expect("if statement checks for some; qed")),
+		// 					length
+		// 				});
+		// 				lexer.consume();
+		// 				array_depth += 1;
+		// 				continue
+		// 			} else {
+		// 				return Err(ErrorKind::UnexpectedToken(lexer.token_as_str().to_owned(), field_type.to_owned()))?
+		// 			}
+		// 		}
+		// 		Token::BracketClose if array_depth == 10 => {
+		// 			return Err(ErrorKind::UnsupportedArrayDepth)?
+		// 		}
+		// 		_  => return Err(ErrorKind::UnexpectedToken(lexer.token_as_str().to_owned(), field_type.to_owned()))?
+		// 	};
 
-// 			token = Some(type_);
-// 			lexer.consume();
-// 		}
+		// 	token = Some(type_);
+		// 	lexer.consume();
+		// }
 
-// 		Ok(token.ok_or_else(|| ErrorKind::NonExistentType)?)
-// 	}
+		Ok(token.ok_or_else(|| ErrorKind::NonExistentType)?)
+	}
 }
